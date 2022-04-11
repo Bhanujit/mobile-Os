253730624a9d465c3f1f8f4f2cb86db3cbdea0e9
Bhanujit Halder <88885935+Bhanujit@users.noreply.github.com>
2022-04-12 00:06:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mobile Operating system</h1>
+      <ul>
+      <li><strong>Androaid</strong></li>
+      <li><strong>Blackberry</strong></li>
+      <li><strong>Iphone</strong></li>
+      <li><strong>Windows Phone</strong></li>
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li><strong>Samsung</strong></li>
+        <li><strong>Apple</strong></li>
+        <li><strong>Micromax</strong></li>
+        <li><strong>Asus</strong></li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
